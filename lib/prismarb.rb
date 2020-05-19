3142d0fba47152f6a06a3bc9515122f21959411c
Uzay-G <uzgirit@gmail.com>
2020-05-19 19:20:16 +0200

    python habitudes

--- lib/prismarb.rb
+++ lib/prismarb.rb
@@ -15,9 +15,9 @@ module Prismarb
     def process_data(message, server)
       message = message.content.split(" ")[0]
       if @commands.has_key? message
-          self.commands[message] += 1
+          @commands[message] += 1
       else
-          self.commands[message] = 1
+          @commands[message] = 1
       end
 
       match = @servers.find { |i| i["name"] == server.name }
@@ -40,13 +40,13 @@ module Prismarb
       processes data and sends every 2 minutes
       TODO: refactor so that users don't need to use ctx
       """
-      unset = False
+      unset = false
       if @time.nil?
           @time = Time.now
-          unset = True
+          unset = true
       end
       server = message.guild
-      self.process_data(message, server)
+      process_data(message, server)
       
 
       curr_time = Time.now
@@ -54,9 +54,9 @@ module Prismarb
       if (unset or interval > 5)
           @time = curr_time
           data = {
-              "commands":  self.commands,
-              "save_server": self.save_server,
-              "servers": self.servers
+              "commands":  @commands,
+              "save_server": @save_server,
+              "servers": @servers
               }
 
           Net::HTTP.post URI('https://prismalytics.herokuapp.com/send_data'),
